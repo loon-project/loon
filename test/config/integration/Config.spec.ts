6d0367d0501302c3f42ee2928b5142a9e336a7bc
vincent178 <vincent.007.cn@gmail.com>
2017-01-20 10:07:59 +0800

    update test

--- test/config/integration/Config.spec.ts
+++ test/config/integration/Config.spec.ts
@@ -7,20 +7,26 @@ import {DIContainer} from "../../../src/di/DIContainer";
 
 describe("Config", () => {
 
-    const path = Path.resolve(process.cwd(), "example/application.json");
+    const path = Path.resolve(process.cwd(), "test/fixture/application.json");
 
     @Config(path)
     class BaseConfig {
 
         @Value("db")
         public db: string;
+
+        @Value("db", {env: false})
+        public testDb: string;
     }
 
 
-    it('should load value from config file to config class', () => {
-
+    it('should load value from config file to config class with env', () => {
         const baseConfig = DIContainer.get(BaseConfig);
-
         baseConfig.db.should.be.equal("mysql");
+    });
+
+    it('should load value from config file to config class without env', () => {
+        const baseConfig = DIContainer.get(BaseConfig);
+        baseConfig.testDb.should.be.equal("redis");
     });
 });
