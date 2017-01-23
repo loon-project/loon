90e8f2f55b6c971fddf043651284a2a66663e603
vincent178 <vincent.007.cn@gmail.com>
2017-01-23 16:56:39 +0800

    fix build

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,7 +3,7 @@ import {DIContainer} from "../di/DIContainer";
 import {TypedConfiguration} from "../server/TypedConfiguration";
 
 export class Log {
-    public static logger = new Winston.Logger(DIContainer.get(TypedConfiguration).logOptions);
+    // public static logger = new Winston.Logger(DIContainer.get(TypedConfiguration).logOptions);
 }
 
 
