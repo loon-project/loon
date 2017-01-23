1a365924b869c8af5484a550dfe34252a86707dc
vincent178 <vincent.007.cn@gmail.com>
2017-01-23 16:50:32 +0800

    add slack integration with travis

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,5 +1,9 @@
-import * as Winston from 'winston';
+import * as Winston from "winston";
+import {DIContainer} from "../di/DIContainer";
+import {TypedConfiguration} from "../server/TypedConfiguration";
 
 export class Log {
-    public static logger = Winston;
+    public static logger = new Winston.Logger(DIContainer.get(TypedConfiguration).logOptions);
 }
+
+
---
+++ src/server/TypedConfiguration.ts
@@ -0,0 +1,8 @@
+
+export abstract class TypedConfiguration {
+
+    protected port: number = 8080;
+
+    protected logOptions: any = {};
+
+}
