a2f727bb46a97da02d10c80c2a53f92a599e5b72
vincent178 <vincent.007.cn@gmail.com>
2017-02-04 15:27:59 +0800

    fix raise exception

--- src/mvc/error/HttpException.ts
+++ src/mvc/error/HttpException.ts
@@ -5,12 +5,4 @@ export abstract class HttpException {
 
     public message: string;
 
-    public stack: string;
-
-    private err: Error;
-
-    constructor() {
-        this.err = new Error();
-        this.stack = this.err.stack ? this.err.stack : "";
-    }
 }
