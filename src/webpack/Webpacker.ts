bf1558df574a7d71f3f282cb6d6473ba85ce5cf0
vincent178 <vincent.007.cn@gmail.com>
2017-02-09 13:04:30 +0800

    extend file for webpacker

--- src/webpack/Webpacker.ts
+++ src/webpack/Webpacker.ts
@@ -1,4 +1,5 @@
 import {TypedContext} from "../server/TypedContext";
+import * as Path from 'path';
 
 export class Webpacker {
 
@@ -7,17 +8,32 @@ export class Webpacker {
     constructor() {
         this.defaultConfig = require('./webpack.config');
         const config: any = TypedContext.getConfig('webpack');
-
-        if (config && config.main) {
-            this.defaultConfig.entry.main = config.main;
-        }
-
-        if (config && config.vendor) {
-            this.defaultConfig.entry.vendor = config.vendor;
-        }
+        this.mergeConfig(config, ['main', 'vendor']);
     }
 
     webpackConfig() {
         return this.defaultConfig;
+    }
+
+    private mergeConfig(config, properties) {
+        properties.forEach(property => {
+            if (config && config[property]) {
+                if (Array.isArray(config[property])) {
+                    this.defaultConfig.entry[property] = config[property].map(this.extendFile);
+                } else {
+                    this.defaultConfig.entry[property] = this.extendFile(config[property]);
+                }
+            }
+
+        });
+
+    }
+
+    private extendFile(file) {
+        if (Path.isAbsolute(file)) {
+            return file;
+        } else {
+            return Path.resolve(TypedContext.assetsDir, file);
+        }
     }
 }
