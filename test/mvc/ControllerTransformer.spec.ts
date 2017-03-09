9af5f87a149d278a785841cef399da763571e290
vincent178 <vincent.007.cn@gmail.com>
2017-03-09 18:30:29 +0800

    add some todo

--- test/mvc/ControllerTransformer.spec.ts
+++ test/mvc/ControllerTransformer.spec.ts
@@ -1,5 +1,26 @@
 import "../TestHelper";
+import {RestController} from "../../src/mvc/decorator/Controller";
+import {Get} from "../../src/mvc/decorator/Method";
+import {ControllerRegistry} from "../../src/mvc/ControllerRegistry";
+import {ControllerTransformer} from "../../src/mvc/ControllerTransformer";
+import * as Express from 'express';
 
 describe('ControllerTransformer', () => {
 
+    @RestController("/abc")
+    class ATestRestControllerForControllerTransformerClass {
+
+        @Get("/")
+        public indexAction() {
+        }
+    }
+
+    it('should convert to router', () => {
+        const controllerMetadata: any = ControllerRegistry.controllers.get(ATestRestControllerForControllerTransformerClass);
+        controllerMetadata.should.not.be.undefined;
+
+        const transformer = new ControllerTransformer(controllerMetadata);
+        transformer.controllerMetadata.should.be.equal(controllerMetadata);
+        transformer.transform().name.should.be.equal(Express.Router().name);
+    });
 });
---
+++ wikis/todo.md
@@ -0,0 +1,7 @@
+1. test controller BeforeAction and AfterAction Middleware
+2. test action BeforeAction and AfterAction Middleware
+3. test action params injected successfully
+4. test controller ErrorAction
+5. test action ErrorAction
+6. test send data successfully
+7. test multiple http method decorated action
