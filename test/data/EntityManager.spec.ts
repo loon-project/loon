eedd026d1ff608c0e472ec46866084ee32f2284b
vincent178 <vincent.007.cn@gmail.com>
2017-03-14 15:38:15 +0800

    add test for EntityManager

--- test/data/EntityManager.spec.ts
+++ test/data/EntityManager.spec.ts
@@ -1,5 +1,27 @@
 import "../TestHelper";
+import {Table} from "../../src/data/decorator/Table";
+import {Column} from "../../src/data/decorator/Column";
+import {EntityManager} from "../../src/data/EntityManager";
 
 describe("EntityManager", () => {
+
+    @Table("users")
+    class User {
+
+        @Column()
+        id: number;
+
+        @Column("created_at")
+        createdAt: Date;
+    }
+
+    it('should successfully get the entity through EntityManager', () => {
+        const userEntity: any = EntityManager.getEntity(User);
+
+        userEntity.table().should.be.equal('users');
+        userEntity.columns().should.be.deep.equal(['users.id AS users.id', 'users.created_at AS users.created_at']);
+        userEntity.column('id').should.be.equal('users.id');
+        userEntity.column('createdAt').should.be.equal('users.created_at');
+    });
 
 });
