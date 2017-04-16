28aec0935e4c63652aa2c0cb7b627d32fa8f68dc
vincent178 <vincent.007.cn@gmail.com>
2017-04-16 20:48:49 +0800

    more tests

--- test/converter/ConverterService.spec.ts
+++ test/converter/ConverterService.spec.ts
@@ -11,43 +11,46 @@ describe('ConverterService', () => {
     class ATestConverterClassTarget {
 
         @ObjectProperty()
-        private name: string;
+        public name: string;
 
         @ObjectProperty("created_at")
-        private createdAt: String;
+        public createdAt: String;
 
         @ObjectProperty("updated_at")
-        private updatedAt: Date;
+        public updatedAt: Date;
     }
+
+    const d1 = new Date();
+    const d2 = new Date();
+
+    const objExample = {
+        name: "aName",
+        created_at: d1.toISOString(),
+        updated_at: d2.toISOString()
+    };
+
+    const insExample = new ATestConverterClassTarget();
+    insExample.name = "FFF";
+    insExample.createdAt = d1.toISOString();
+    insExample.updatedAt = d2;
 
 
     it('should convert data to String', () => {
-
-        assertAlot(String, [
-            'aaa',
-            [111, '111']
-        ]);
-
+        assertConverterResult(String, 'aaa');
+        assertConverterResult(String, 111, '111');
     });
 
     it('should convert data to Number', () => {
-
-        assertAlot(Number, [
-            111
-        ]);
-
+        assertConverterResult(Number, 111);
     });
 
     it('should convert data to Boolean', () => {
-
-        assertAlot(Boolean, [
-            ['true', true],
-            ['false', false],
-            true, false,
-            [1, true],
-            [0, false]
-        ]);
-
+        assertConverterResult(Boolean, 'true', true);
+        assertConverterResult(Boolean, 'false', false);
+        assertConverterResult(Boolean, true);
+        assertConverterResult(Boolean, false);
+        assertConverterResult(Boolean, 1, true);
+        assertConverterResult(Boolean, 0, false);
     });
 
     it('should convert data to Date', () => {
@@ -56,56 +59,90 @@ describe('ConverterService', () => {
         const nowString = now.toISOString();
         const nowNumber = now.getTime();
 
-        assertAlot(Date, [
-            now,
-            [nowString, now],
-            [nowNumber, now]
-        ]);
-
+        assertConverterResult(Date, now);
+        assertConverterResult(Date, nowString, now);
+        assertConverterResult(Date, nowNumber, now);
     });
 
     it('should convert an object to a class instance', () => {
+        const ins = converter.convert(objExample, ATestConverterClassTarget);
+        assertInstance(ins);
+    });
 
-        const now = new Date();
+    it('should convert an instance to an object', () => {
+        const obj = converter.convert(insExample, Object);
+        assertObject(obj);
+    });
 
-        const obj = {
-            name: "aName",
-            created_at: now.toISOString(),
-            updated_at: now.toISOString()
-        };
+    it('should convert an array of objects to an array of class instance', () => {
+        const objArr = [objExample, objExample, objExample];
+        const insArr = converter.convert(objArr, Array, ATestConverterClassTarget);
 
-        const ins = converter.convert(obj, ATestConverterClassTarget);
+        expect(insArr instanceof Array).to.be.true;
+        expect(insArr.length).to.be.equal(3);
 
-        expect(ins instanceof ATestConverterClassTarget).to.be.true;
-        expect(ins.name).to.be.equal('aName');
-        expect(ins.createdAt).to.be.equal(now.toISOString());
-        expect(ins.updatedAt.getTime()).to.be.equal(now.getTime());
+        insArr.forEach(assertInstance);
+    });
+
+    it('should convert an array of class instance to an array of objects', () => {
+        const insArr = [insExample, insExample, insExample];
+        const objArr = converter.convert(insArr, Array, Object);
+
+        expect(objArr instanceof Array).to.be.true;
+        expect(objArr.length).to.be.equal(3);
+
+        objArr.forEach(assertObject);
+    });
+
+    it('should convert a map with object as value to a map with class instance as value', () => {
+        const map = new Map();
+        map.set('key', objExample);
+
+        const convertedMap = converter.convert(map, Map, ATestConverterClassTarget);
+
+        expect(convertedMap instanceof Map).to.be.true;
+
+        const ins = convertedMap.get('key');
+        assertInstance(ins);
+    });
+
+    it('should convert a map with class instance as value to a map with object as value', () => {
+        const map = new Map();
+        map.set('key', insExample);
+
+        const convertedMap = converter.convert(map, Map, Object);
+
+        expect(convertedMap instanceof Map).to.be.true;
+
+        const obj = convertedMap.get('key');
+        assertObject(obj);
     });
 
 
-    function assertAlot(type: Function, data: any[]) {
 
-        data.forEach(item => {
+    function assertConverterResult(type: Function, data: any, target?: any) {
 
-            if (item instanceof Array) {
-                const [input, output] = item;
-                assertSerializeAndDeserialize(input, type, output);
-            } else {
-                assertSerializeAndDeserialize(item, type, item);
-            }
-
-        });
-    }
-
-
-    function assertSerializeAndDeserialize(input: any, type: Function, expectOutput: any) {
-
-        if (type.name === 'Date') {
-            expect(converter.convert(input, type).getTime()).to.be.equal(expectOutput.getTime());
-        } else {
-            expect(converter.convert(input, type)).to.be.equal(expectOutput);
+        if (typeof target === 'undefined') {
+            target = data;
         }
 
+        if (type === Date) {
+            expect(converter.convert(data, type).getTime()).to.be.equal(target.getTime());
+        } else {
+            expect(converter.convert(data, type)).to.be.equal(target);
+        }
+    }
 
+    function assertInstance(ins) {
+        expect(ins).not.to.be.undefined;
+        expect(ins.name).to.be.equal('aName');
+        expect(ins.createdAt).to.be.equal(d1.toISOString());
+        expect(ins.updatedAt.getTime()).to.be.equal(d2.getTime());
+    }
+
+    function assertObject(obj) {
+        expect(obj.name).to.be.equal('FFF');
+        expect(obj.created_at).to.be.equal(d1.toISOString());
+        expect(obj.updated_at.getTime()).to.be.equal(d2.getTime());
     }
 });
