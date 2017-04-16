ab02c236e6ace99a76757f0e3a96d91b281d627a
vincent178 <vincent.007.cn@gmail.com>
2017-04-16 11:49:06 +0800

    clean up

--- src/converter/JsonSourceMetadata.ts
+++
@@ -1,9 +0,0 @@
-import {JsonPropertyMetadata} from "./JsonPropertyMetadata";
-
-export class JsonSourceMetadata {
-
-    private _properties: Map<string, JsonPropertyMetadata> = new Map();
-
-    public properties = this._properties;
-
-}
