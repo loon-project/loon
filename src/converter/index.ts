7fac33ecf84f5493b6ef64f83fd569b6ed43f1c1
vincent178 <vincent.007.cn@gmail.com>
2017-04-18 17:23:47 +0800

    add export converter

--- src/converter/index.ts
+++ src/converter/index.ts
@@ -2,3 +2,6 @@
 export * from './Converter';
 export * from './ConvertOptions'
 export * from './ConvertTemplate';
+
+export * from './ConverterService';
+export * from './decorator/ObjectProperty';
