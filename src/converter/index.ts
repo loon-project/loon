6a6188d01979938e920031c0d5615379e0b2b712
vincent178 <vincent.007.cn@gmail.com>
2017-05-10 20:20:34 +0800

    expose IConverter

--- src/converter/index.ts
+++ src/converter/index.ts
@@ -3,3 +3,4 @@ export * from './ConverterService';
 export * from './decorator/Property';
 export * from './decorator/PropertyInherited';
 export * from './PropertyRegistry';
+export * from './interface/IConverter';
