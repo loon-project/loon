fdf38f53861a42985609a4708cd3a6cd1910a204
vincent178 <vh7157@gmail.com>
2018-07-13 09:03:44 +0800

    remove event system

--- src/event/decorator/On.ts
+++
@@ -1,9 +0,0 @@
-import {EventEmitter} from "events";
-import {EventListenerRegistry} from "../EventListenerRegistry";
-
-export function On(eventName: string, eventEmitterKlass: any) {
-    return (target: any, key: string, descriptor) => {
-        const handler = descriptor.value;
-        EventListenerRegistry.registerEventListener(target.constructor, key, eventName, eventEmitterKlass, handler);
-    };
-}
--- src/event/index.ts
+++
@@ -1,3 +0,0 @@
-
-export * from './decorator/On';
-export * from './decorator/Subscriber';
